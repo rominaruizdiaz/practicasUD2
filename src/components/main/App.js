75ccea62beeeff68f5f151b96ab6cc1e9faafb79
Romina-Ruiz <ruizdiazsanchezromina@gmail.com>
2025-10-22 12:44:50 +0200

    add: nuevo componente con state

--- src/components/main/App.js
+++ src/components/main/App.js
@@ -12,11 +12,7 @@ function App() {
       <div>
         <p>Bienvenido a mi aplicación, esto fue creado con Javascript en React.</p>
       </div>
-      <MiLista titulo="Clientes" nombre1="Juan" nombre2="Iker" nombre3="Elisa" nombre4="Manuel"/>
-      <br/>
-      <MiLista titulo="Amigos"  nombre1="Nico" nombre2="Inas" nombre3="Juan" nombre4="Elisse"/>
-      <br/>
-      <MiLista titulo="Familiares"  nombre1="Carolina" nombre2="Pablo" nombre3="Irene" nombre4="Paula"/>
+      <MiLista />
       <Footer/>
 
     </div>
--- src/components/miLista/miLista.js
+++ src/components/miLista/miLista.js
@@ -1,19 +1,61 @@
 import './miLista.css';
+import React from "react";
 
 //función que siempre retorna un único componente
-function miLista(props){
-    return(
-        //se puede poner <></> que se llaman fragmentos, o dentro de un <div></div>
-        <>
-            <h4>Mi lista de {props.titulo}</h4>
-            <ul>
-                <li>{props.nombre1}</li>
-                <li>{props.nombre2}</li>
-                <li>{props.nombre3}</li>
-                <li>{props.nombre4}</li>
-            </ul>
-        </>
-    )
+class miLista extends React.Component{
+    //variable de estado para que se puedan modificar las incidencias
+
+    state = {incidencias :[
+    {
+        id_incidencia:1,
+        id_usuario: 'e768590345h',
+        titulo: 'Proyector averiado en aula 2',
+        descripcion: 'El proyector no enciende ',
+        categoria: 'Hardware',
+        nivel_urgencia: 'Alta',
+        fecha_registro: '2025-20-03',
+        estado: 'Abierto',
+        ubicacion: 'A301'  
+    },    
+    {
+        id_incidencia:2,
+        id_usuario: 'g138204639a',
+        titulo: 'Proyector averiado en aula 2',
+        descripcion: 'El proyector no enciende ',
+        categoria: 'Hardware',
+        nivel_urgencia: 'Alta',
+        fecha_registro: '2025-20-03',
+        estado: 'Abierto',
+        ubicacion: 'B301'   
+    },    
+    {
+        id_incidencia:3,
+        id_usuario: 'j3529056483t',
+        titulo: 'Proyector averiado en aula 2',
+        descripcion: 'El proyector no enciende ',
+        categoria: 'Hardware',
+        nivel_urgencia: 'Alta',
+        fecha_registro: '2025-20-03',
+        estado: 'Abierto',
+        ubicacion: 'C301'  
+    }
+    ]};
+    render(){
+
+        return(
+            //se puede poner <></> que se llaman fragmentos, o dentro de un <div></div>
+            <>
+                <ul>
+                    {this.state.incidencias.map((i) => (
+                        <li key={i.id_incidencia}><strong>Titulo:</strong> {i.titulo}, <br/>
+                        <strong>Descripción:</strong> {i.descripcion},<br/>
+                        <strong>Usuario:</strong> {i.id_usuario}, <br/>
+                        <strong>Ubicación:</strong> {i.ubicacion},<br/><br/> </li>
+                    ))}
+                </ul>
+            </>
+        )
+    }
 }
 
 //por defecto debe tener
