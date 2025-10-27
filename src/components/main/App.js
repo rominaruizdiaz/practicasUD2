583800a1f16b9e417ca2c043bdbabf3bfef2b5fb
Romina-Ruiz <ruizdiazsanchezromina@gmail.com>
2025-10-27 14:13:35 +0100

    add: datos unidireccionales

--- src/components/main/App.js
+++ src/components/main/App.js
@@ -1,22 +1,64 @@
 import './App.css';
+import React from 'react';
 import MiLista from '../miLista/miLista.js';
 import Header from '../header/Header.js';
 import Footer from '../footer/Footer.js';
 
-function App() {
-  return (
-    <div>
-      <Header/>
-      <h1>Hola mundo</h1>
-      <h2>Este es mi primer componente React</h2>
-      <div>
-        <p>Bienvenido a mi aplicación, esto fue creado con Javascript en React.</p>
-      </div>
-      <MiLista />
-      <Footer/>
+class App extends React.Component {
+  
+  state = {
+    incidencias: [
+      {
+        id_incidencia: 1,
+        id_usuario: 'e768590345h',
+        titulo: 'Proyector averiado en aula 2',
+        descripcion: 'El proyector no enciende',
+        categoria: 'Hardware',
+        nivel_urgencia: 'Alta',
+        fecha_registro: '2025-03-20',
+        estado: 'Abierto',
+        ubicacion: 'A301'
+      },
+      {
+        id_incidencia: 2,
+        id_usuario: 'g138204639a',
+        titulo: 'Pantalla rota en aula 3',
+        descripcion: 'La pantalla está dañada y no muestra imagen',
+        categoria: 'Hardware',
+        nivel_urgencia: 'Alta',
+        fecha_registro: '2025-03-20',
+        estado: 'Abierto',
+        ubicacion: 'B301'
+      },
+      {
+        id_incidencia: 3,
+        id_usuario: 'j3529056483t',
+        titulo: 'Problemas con conexión Wi-Fi',
+        descripcion: 'La red no se conecta correctamente en el aula C301',
+        categoria: 'Red',
+        nivel_urgencia: 'Media',
+        fecha_registro: '2025-03-20',
+        estado: 'Abierto',
+        ubicacion: 'C301'
+      }
+    ]
+  };
 
-    </div>
-  );
+  render(){
+    return (
+      <div>
+        <Header/>
+        <h1>Hola mundo</h1>
+        <h2>Este es mi primer componente React</h2>
+        <div>
+          <p>Bienvenido a mi aplicación, esto fue creado con Javascript en React.</p>
+        </div>
+        <MiLista incidencias={this.state.incidencias}/>
+        <Footer/>
+  
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/components/miLista/miLista.js
+++ src/components/miLista/miLista.js
@@ -5,48 +5,13 @@ import React from "react";
 class miLista extends React.Component{
     //variable de estado para que se puedan modificar las incidencias
 
-    state = {incidencias :[
-    {
-        id_incidencia:1,
-        id_usuario: 'e768590345h',
-        titulo: 'Proyector averiado en aula 2',
-        descripcion: 'El proyector no enciende ',
-        categoria: 'Hardware',
-        nivel_urgencia: 'Alta',
-        fecha_registro: '2025-20-03',
-        estado: 'Abierto',
-        ubicacion: 'A301'  
-    },    
-    {
-        id_incidencia:2,
-        id_usuario: 'g138204639a',
-        titulo: 'Proyector averiado en aula 2',
-        descripcion: 'El proyector no enciende ',
-        categoria: 'Hardware',
-        nivel_urgencia: 'Alta',
-        fecha_registro: '2025-20-03',
-        estado: 'Abierto',
-        ubicacion: 'B301'   
-    },    
-    {
-        id_incidencia:3,
-        id_usuario: 'j3529056483t',
-        titulo: 'Proyector averiado en aula 2',
-        descripcion: 'El proyector no enciende ',
-        categoria: 'Hardware',
-        nivel_urgencia: 'Alta',
-        fecha_registro: '2025-20-03',
-        estado: 'Abierto',
-        ubicacion: 'C301'  
-    }
-    ]};
     render(){
 
         return(
             //se puede poner <></> que se llaman fragmentos, o dentro de un <div></div>
             <>
                 <ul>
-                    {this.state.incidencias.map((i) => (
+                    {this.props.incidencias.map((i) => (
                         <li key={i.id_incidencia}><strong>Titulo:</strong> {i.titulo}, <br/>
                         <strong>Descripción:</strong> {i.descripcion},<br/>
                         <strong>Usuario:</strong> {i.id_usuario}, <br/>
