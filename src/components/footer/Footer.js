0107a4a127a697af2a5ea627f0d49106e992b982
Romina-Ruiz <ruizdiazsanchezromina@gmail.com>
2025-11-23 22:49:06 +0100

    change: logo del ies y cambio de texto en el footer

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -4,7 +4,7 @@ class Footer extends React.Component {
     render() {
         return (
             <div className="bg-dark text-white text-center p-3">
-                <h3>Lorem ipsum</h3>
+                <h3>Copyright © 2025 Romina</h3>
             </div>
         );
     }
--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,10 +1,12 @@
 import React from "react";
+import Logo from "../../img/logoIES.png";
 
 class Header extends React.Component {
     render() {
         return (
             <div className="bg-dark text-white text-center p-3">
-                <h3>Bienvenido a la página de contactos.</h3>
+                <img src={Logo} alt="Logo del IES" width="100px" />
+                <h3>Bienvenido a la página de incidencias</h3>
             </div>
         );
     }
