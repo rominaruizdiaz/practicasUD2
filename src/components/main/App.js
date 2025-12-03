6f650f3ed6901289ca48a24aa279115a414a5747
Romina-Ruiz <ruizdiazsanchezromina@gmail.com>
2025-12-03 13:45:35 +0100

    add: funciones para la carga GET de Usuarios e Incidencias

--- src/components/main/App.js
+++ src/components/main/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 
 import MiLista from "../miLista/miLista.js";
 import Formu from "../form/Formu.js";
@@ -8,41 +8,44 @@ import Footer from "../footer/Footer.js";
 import Fondo from "../../img/fondo.png";
 
 function App() {
-  const [incidencias, setIncidencia] = useState([
-    {
-      id_incidencia: 1,
-      id_usuario: "e768590345h",
-      titulo: "Proyector averiado en aula 2",
-      descripcion: "El proyector no enciende",
-      categoria: "Hardware",
-      nivel_urgencia: "Alta",
-      fecha_registro: "2025-03-20",
-      estado: "Abierto",
-      ubicacion: "A301",
-    },
-    {
-      id_incidencia: 2,
-      id_usuario: "g138204639a",
-      titulo: "Pantalla rota en aula 3",
-      descripcion: "La pantalla está dañada y no muestra imagen",
-      categoria: "Hardware",
-      nivel_urgencia: "Alta",
-      fecha_registro: "2025-03-20",
-      estado: "Abierto",
-      ubicacion: "B301",
-    },
-    {
-      id_incidencia: 3,
-      id_usuario: "j3529056483t",
-      titulo: "Problemas con conexión Wi-Fi",
-      descripcion: "La red no se conecta correctamente en el aula C301",
-      categoria: "Red",
-      nivel_urgencia: "Media",
-      fecha_registro: "2025-03-20",
-      estado: "Abierto",
-      ubicacion: "C301",
-    },
-  ]);
+  const INCIDENCIA_API_URL = 'http://localhost:3004/incidencias';
+  const USUARIO_API_URL = 'http://localhost:3004/users';
+
+  const [usuarios, setUsuarios] = useState([]);
+  const [incidencias, setIncidencia] = useState([]);
+
+  useEffect (()=>{
+
+    const obtenerIncidencias = async () => {
+      try{
+        let response = await fetch (INCIDENCIA_API_URL);
+        if(!response.ok){
+          throw new Error("HTTP Error");
+        }
+        const data = await response.json();
+        console.log(data);
+        setIncidencia(data);
+      } catch(e){
+        console.error("Error al cargar las incidencias:", e);
+      }
+    }
+    const obtenerUsuarios = async () => {
+      try{
+        let response = await fetch (USUARIO_API_URL);
+        if(!response.ok){
+          throw new Error("HTTP Error");
+        }
+        const data = await response.json();
+        console.log(data);
+        setUsuarios(data);
+      } catch(e){
+        console.error("Error al cargar los usuarios:", e);
+      }
+    }
+
+    obtenerIncidencias();
+    obtenerUsuarios();
+  },[]);
 
   const agregarIncidencia = (
     nuevo_usuario,
